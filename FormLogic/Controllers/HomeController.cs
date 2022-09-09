b603b1e4f53bc02fc6b9bdcca4e0cb5177a6b610
洛神赋 <hluo5206@gmail.com>
2022-09-09 08:46:12 +0800

    assets: 添加资源文件feat: FormLogic

---
+++ FormLogic/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using FormLogic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FormLogic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string name, int id)
+        {
+            ViewData["NAME"] = "Hello " + name;
+            ViewData["UUID"] = "ID " + id;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
