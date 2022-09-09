a8572ffee53b6b2fcc29447a290039f8f48ad7d6
洛神赋 <hluo5206@gmail.com>
2022-09-09 08:46:12 +0800

    feat: FormLogic

---
+++ FormLogic/Data/FormLogicContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FormLogic.Models;
+
+namespace FormLogic.Data
+{
+    public class FormLogicContext : DbContext // 数据库上下文类
+    {
+        public FormLogicContext (DbContextOptions<FormLogicContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FormLogic.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ FormLogic/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace FormLogic.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
